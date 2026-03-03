403d23e4ee46c3070ec3f0b1b1480fff3cfe5952
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-03-03 18:53:20 +0100

    add newsletter w/ metadata

--- demo/src/app/pages/index.page.ts
+++ demo/src/app/pages/index.page.ts
@@ -2,17 +2,18 @@ import { Component } from '@angular/core';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 
 import { Newsletter } from '../ui/newsletter';
+import { NewsletterMetadata } from '../ui/newsletter-metadata';
 
 @Component({
   selector: 'app-home',
-  imports: [Newsletter, HlmButtonImports],
+  imports: [Newsletter, NewsletterMetadata, HlmButtonImports],
   template: `
-    <div>
-      <h1>Welcome to the Better Auth Plugins Demo!</h1>
-      <button hlmBtn>Click me</button>
+    <div class="px-4 py-16">
+      <h1 class="text-4xl font-bold">Better Auth Plugins - Demo</h1>
     </div>
 
     <ba-newsletter />
+    <ba-newsletter-metadata />
   `,
 })
 export default class Home {}
