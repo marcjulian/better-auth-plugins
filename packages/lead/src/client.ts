49bc384a7fc88f2647317bd16427d477e5737f0d
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-04-14 10:38:25 +0200

    fix: reduce client bundle size, closes #5

--- packages/lead/src/client.ts
+++ packages/lead/src/client.ts
@@ -1,12 +1,10 @@
 import type { BetterAuthClientPlugin } from 'better-auth/client';
 
-import { LEAD_ERROR_CODES } from './error-codes';
 import type { lead } from './index';
 
 export const leadClient = () => {
   return {
     id: 'lead',
     $InferServerPlugin: {} as ReturnType<typeof lead>,
-    $ERROR_CODES: LEAD_ERROR_CODES,
   } satisfies BetterAuthClientPlugin;
 };
