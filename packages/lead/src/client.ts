0be129caee656d9a0f6d2ad46120983124c9c36c
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-04-14 11:00:14 +0200

    update client type

--- packages/lead/src/client.ts
+++ packages/lead/src/client.ts
@@ -2,9 +2,11 @@ import type { BetterAuthClientPlugin } from 'better-auth/client';
 
 import type { lead } from './index';
 
+type LeadPlugin = typeof lead;
+
 export const leadClient = () => {
   return {
     id: 'lead',
-    $InferServerPlugin: {} as ReturnType<typeof lead>,
+    $InferServerPlugin: {} as ReturnType<LeadPlugin>,
   } satisfies BetterAuthClientPlugin;
 };
